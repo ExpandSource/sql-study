0853c5abdfe3ee895fcb04ae6f30e2c70d51d624
expandsource <expandsource@gmail.com>
2024-04-04 10:05:50 +0900

    제약조건 이름 명시하기 및 확인하기

--- 테이블생성_수정_삭제/테이블 구조변경(ALTER).sql
+++ 테이블생성_수정_삭제/테이블 구조변경(ALTER).sql
@@ -31,11 +31,13 @@ ALTER TABLE employees_backup RENAME TO employees;
 DESCRIBE employees;
 
 -- 제약조건 추가하기
+-- 1. 행 추가
 ALTER TABLE employees ADD COLUMN email VARCHAR(255); 
 
--- UNIQUE 제약조건 추가
+-- 2. UNIQUE 제약조건 추가
 ALTER TABLE employees ADD UNIQUE (email);
 
+DESCRIBE employees;
 -- 외래키 제약조건 추가
 -- 1. 참조할 테이블 생성
 CREATE TABLE departments (
@@ -64,10 +66,10 @@ CHECK (age > 19);
 ALTER TABLE employees ADD COLUMN salary INT COMMENT '직원의 급여와 관련된 속성';
 
 -- 제약조건 삭제하기
--- 1. 외래키 삭제하기
+-- 1. 외래키 삭제하기 (제약조건의 이름으로 삭제)
 ALTER TABLE employees DROP FOREIGN KEY fk_department_id;
 
--- 2. 체크 명시 제약조건 삭제하기
+-- 2. 체크 명시 제약조건 삭제하기 (제약조건의 이름으로 삭제)
 ALTER TABLE employees DROP CONSTRAINT chk_age;
 
 -- ALTER COLUMN 으로 열 속성 변경하기
@@ -75,3 +77,17 @@ ALTER TABLE employees DROP CONSTRAINT chk_age;
 ALTER TABLE employees ALTER COLUMN age SET DEFAULT 20;
 -- 2. 기본 값 삭제
 ALTER TABLE employees ALTER COLUMN age DROP DEFAULT;
+ 
+-- 제약조건 이름 확인하기
+-- 1. information_schema 오브젝트를 통해 확인
+-- CONTRAINT_NAME 필드 : 제약조건의 이름
+SELECT * FROM information_schema.table_constraints
+WHERE table_name = 'employees';  -- 테이블명
+
+-- 2. DDL을 통해 확인
+-- SHOW CREATE TABLE 스키마명.테이블명;
+SHOW CREATE TABLE employees;
+
+
+
+
