08c065b940eb9f4de2027cfecaac239dfbde5b34
expandsource <expandsource@gmail.com>
2024-04-08 16:26:03 +0900

    데이터 결합 JOIN

---
+++ 데이터조회(SELECT)/연습문제7.sql
@@ -0,0 +1,11 @@
+-- JOIN
+-- employees 데이터베이스에서 연습해보세요.
+-- employees(first_name, last_name, emp_no), titles(emp_no, title, to_date)
+-- dept_emp(dept_no, emp_no, to_date), salaries(emp_no, salary, to_date)
+-- departments(dept_no, dept_name)
+
+-- 문제 1: 직원의 이름(first_name, last_name)과 직원의 현재 직책(title)을 조회하세요.
+-- 문제 2: 각 부서의 이름(dept_name)과 그 부서에 현재 속해있는 직원 수를 조회하세요.
+-- 문제 3: 현재 직원 중 급여가 80000 이상인 직원의 이름(first_name, last_name)과 급여(salary)를 조회하세요.
+-- 문제 4: 'Marketing' 부서에 현재 속해있는 직원의 이름(first_name, last_name)과 부서 이름(dept_name)을 조회하세요.
+-- 문제 5: 각 직원의 이름과 해당 직원이 현재 속한 부서 이름을 조회하세요. (LEFT JOIN 사용)
